ffbbecb0a450a60bdb42dbdeb80dad970341f4ae
sk7001 <sk7001@srmist.edu.in>
2024-08-07 21:27:21 +0530

    removed db as theres no use

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv").config()
-const connect_db = require("./connectdb/connectDb")
 const app = express()
 app.use(cors())
 app.use(express.json({limit: '10mb'}))
 
 
 app.listen(process.env.PORT,async ()=>{
-    await connect_db()    
     console.log(`Server is up on port ${process.env.PORT}.`);
 })
 
